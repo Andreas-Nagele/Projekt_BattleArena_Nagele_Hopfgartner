ae1e8b69f816595a7215624488069009edf67910
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-02-19 11:07:06 +0100

    Add attributes and getter/Setter

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Character.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Character.java
@@ -1,5 +1,35 @@
 package battleArena;
 
 public class Character {
+	private String name;
+	private int attackStrength;
+	private int hearts;
+	private boolean specialAbilityActive;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAttackStrength() {
+		return attackStrength;
+	}
+	public void setAttackStrength(int attackStrength) {
+		this.attackStrength = attackStrength;
+	}
+	public int getHearts() {
+		return hearts;
+	}
+	public void setHearts(int hearts) {
+		this.hearts = hearts;
+	}
+	public boolean isSpecialAbilityActive() {
+		return specialAbilityActive;
+	}
+	public void setSpecialAbilityActive(boolean specialAbilityActive) {
+		this.specialAbilityActive = specialAbilityActive;
+	}
 
 }
