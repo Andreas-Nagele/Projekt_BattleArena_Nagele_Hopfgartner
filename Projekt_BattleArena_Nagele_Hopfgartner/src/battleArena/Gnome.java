9bfd04abf0d50fe6e0944b1185fda5e6a3c35aed
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-02-19 11:20:57 +0100

    added constructor

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Gnome.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Gnome.java
@@ -1,5 +1,14 @@
 package battleArena;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Gnome extends Character{
 
+
+	public Gnome(String name) {
+		this.setName(name);
+		this.setAttackStrength(ThreadLocalRandom.current().nextInt(15, 26));
+		this.setHearts(100);
+		this.setSpecialAbilityActive(false);
+	}
 }
