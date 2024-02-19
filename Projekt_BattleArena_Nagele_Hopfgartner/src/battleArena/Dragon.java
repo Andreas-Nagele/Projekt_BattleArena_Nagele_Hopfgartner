22e758db4938c08a1f2b8b57348cefcafe759a03
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-02-19 11:15:51 +0100

    Add constructor and start with activateSpecialAbility method

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Dragon.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Dragon.java
@@ -1,5 +1,17 @@
 package battleArena;
 
-public class Dragon {
+import java.util.concurrent.ThreadLocalRandom;
 
+public class Dragon extends Character{
+
+	public Dragon(String name) {
+		this.setName(name);
+		this.setAttackStrength(ThreadLocalRandom.current().nextInt(20, 26));
+		this.setHearts(100);
+		this.setSpecialAbilityActive(false);
+	}
+	
+	public void activateSpecialAbility() {
+		this.setAttackStrength(this.getAttackStrength() - ThreadLocalRandom.current().nextInt(5, 11));
+	}
 }
