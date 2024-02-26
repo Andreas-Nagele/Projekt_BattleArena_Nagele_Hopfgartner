dda0bf28ed09ee97b1d0b8eaa7fc2d0bf7187015
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-02-26 12:01:13 +0100

    Create class BattleArena

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/BattleArena.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/BattleArena.java
@@ -1,5 +1,36 @@
 package battleArena;
 
 public class BattleArena {
+	
+	private Character c1;
+	private Character c2;
+	private Character winner;
+	
+	public BattleArena(Character c1, Character c2) {
+		this.c1 = c1;
+		this.c2 = c2;
+		this.winner = null;
+	}
+
+	public Character getC1() {
+		return c1;
+	}
+	public void setC1(Character c1) {
+		this.c1 = c1;
+	}
+	public Character getC2() {
+		return c2;
+	}
+	public void setC2(Character c2) {
+		this.c2 = c2;
+	}
+	public Character getWinner() {
+		return winner;
+	}
+	public void setWinner(Character winner) {
+		this.winner = winner;
+	}
+	
+	
 
 }
