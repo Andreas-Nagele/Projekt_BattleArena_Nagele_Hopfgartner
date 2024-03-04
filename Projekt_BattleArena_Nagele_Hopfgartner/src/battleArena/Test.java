32e1edab2745443bf314c8ccffe6706e6c120bbf
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-03-04 10:11:09 +0100

    Merge branch 'master' of https://github.com/Andreas-Nagele/Projekt_BattleArena_Nagele_Hopfgartner.git

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
@@ -2,12 +2,14 @@ package battleArena;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		Character c = new Dragon("Willhelm");
 		System.out.println(c);
 		Character c2 = new Gnome("Constantin");
 		System.out.println(c2);
+		
+		BattleArena b = new BattleArena(c, c2);
+		b.fight();
 	}
 
 }
