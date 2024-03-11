bb753a1b46f154b27c35d9db5a5a9969b8db780d
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-03-11 10:28:17 +0100

    Begin with init method

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
@@ -4,6 +4,28 @@ import java.util.Scanner;
 
 public class Test {
 
+	public static void init(BattleArena b) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Spieler 1, bitte wähle deine Charakterart!");
+		String c1 = sc.nextLine();
+		System.out.println("Spieler 1, bitte wähle deine Charakterart!");
+		System.out.println("Spieler 2, bitte wähle deinen Character!");
+		String c2 = sc.nextLine();
+		do {
+			for (Character c : b.getList()) {
+				if (c.getName().equalsIgnoreCase(c1)) {
+					b.setC1(c);
+				}
+			}
+		}while (b.getC1() == null);
+		
+		for (Character c : b.getList()) {
+			if(c.getName().equalsIgnoreCase(c2)) {
+				b.setC2(c);
+			}
+		}
+	}
+	
 	public static void main(String[] args) {	
 		Scanner sc = new Scanner(System.in);
 		
@@ -12,13 +34,10 @@ public class Test {
 		Character c3 = new Gnome("Constantin");
 		System.out.println(c3);
 		
-		System.out.println("Spieler 1, bitte wähle deinen Character!");
-		String c1 = sc.nextLine();
-		System.out.println("Spieler 2, bitte wähle deinen Character!");
-		String c2 = sc.nextLine();
 		
 		
-		BattleArena b = new BattleArena(c, c3);
+		BattleArena b = new BattleArena(null, null);
+		init(b);
 		b.fight();
 		
 		sc.close();
