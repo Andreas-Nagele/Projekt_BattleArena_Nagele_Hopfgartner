c214fe7f1d5f21c8ff1ded7c8b750f5030efbfdf
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-11 10:25:01 +0100

    Added characters for Gnome and Dragon

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
@@ -7,10 +7,30 @@ public class Test {
 	public static void main(String[] args) {	
 		Scanner sc = new Scanner(System.in);
 		
-		Character c = new Dragon("Willhelm");
+		//Dragon character
+		Character w = new Dragon("Willhelm");
+		System.out.println(w);
+		Character g = new Dragon("Gerald");
+		System.out.println(g);
+		Character j = new Dragon("Josef");
+		System.out.println(j);
+		Character a = new Dragon("Andreas");
+		System.out.println(a);
+		Character m = new Dragon("Mustafa");
+		System.out.println();
+		
+		//Gnome character
+		Character c = new Gnome("Constantin");
 		System.out.println(c);
-		Character c3 = new Gnome("Constantin");
-		System.out.println(c3);
+		Character l = new Gnome("Lyon");
+		System.out.println(l);
+		Character o = new Gnome("Otto");
+		System.out.println(o);
+		Character v = new Gnome("Vitkor");
+		System.out.println(v);
+		Character k = new Gnome("Kutay");
+		System.out.println(k);
+		
 		
 		System.out.println("Spieler 1, bitte wähle deinen Character!");
 		String c1 = sc.nextLine();
@@ -18,7 +38,7 @@ public class Test {
 		String c2 = sc.nextLine();
 		
 		
-		BattleArena b = new BattleArena(c, c3);
+		BattleArena b = new BattleArena(w, c);
 		b.fight();
 		
 		sc.close();
