89046efba9dd248b99ade3af98531d7dd6fb4c9e
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-03-11 10:18:52 +0100

    Choose your character via scanner

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
@@ -1,15 +1,27 @@
 package battleArena;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {	
+		Scanner sc = new Scanner(System.in);
+		
 		Character c = new Dragon("Willhelm");
 		System.out.println(c);
-		Character c2 = new Gnome("Constantin");
-		System.out.println(c2);
+		Character c3 = new Gnome("Constantin");
+		System.out.println(c3);
 		
-		BattleArena b = new BattleArena(c, c2);
+		System.out.println("Spieler 1, bitte wähle deinen Character!");
+		String c1 = sc.nextLine();
+		System.out.println("Spieler 2, bitte wähle deinen Character!");
+		String c2 = sc.nextLine();
+		
+		
+		BattleArena b = new BattleArena(c, c3);
 		b.fight();
+		
+		sc.close();
 	}
 
 }
