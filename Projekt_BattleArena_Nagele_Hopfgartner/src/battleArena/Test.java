2e0eef94fff00a1bd4bb00c9d0d1976fdfb6ec60
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-03-11 10:53:38 +0100

    Remove init method and move it to fight()

--- Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
+++ Projekt_BattleArena_Nagele_Hopfgartner/src/battleArena/Test.java
@@ -3,49 +3,12 @@ package battleArena;
 import java.util.Scanner;
 
 public class Test {
-
-	public static void init(BattleArena b) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Spieler 1, bitte wähle deine Charakterart! 1 = Dragon, 2 = Gnome");
-		int c1 = sc.nextInt();
-		System.out.println("Spieler 1, bitte wähle deinen Charakternamen!");
-		String c11 = sc.next();
-		System.out.println("Spieler 2, bitte wähle deine Charakterart! 1 = Dragon, 2 = Gnome");
-		int c2 = sc.nextInt();
-		System.out.println("Spieler 2, bitte wähle deinen Charakternamen!");
-		String c22 = sc.next();
-		
-		switch(c1) {
-			case 1:
-				Character d1 = new Dragon(c11);
-				b.setC1(d1);
-				break;
-			case 2:
-				Character g1 = new Gnome(c11);
-				b.setC1(g1);
-				break;
-		}
-		
-		switch(c2) {
-			case 1:
-				Character d2 = new Dragon(c22);
-				b.setC2(d2);
-				break;
-			case 2:
-				Character g2 = new Gnome(c22);
-				b.setC2(g2);
-				break;
-		}
-		
-		sc.close();
-	}
 	
 	public static void main(String[] args) {	
 		Scanner sc = new Scanner(System.in);
 		
 		BattleArena b = new BattleArena(null, null);
-		init(b);
+		
 		b.fight();
 
 		sc.close();
